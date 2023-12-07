d0c0593ff4c1e32a7480d717904083fc23565cb7
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-07 23:30:48 +0500

    add class

--- 8kyu/Profile Lookup.js
+++ 8kyu/Profile Lookup.js
@@ -29,14 +29,16 @@ const contacts = [
 function lookUpProfile(name, prop) {
     // Only change code below this line
     for (let i = 0; i < contacts.length; i++) {
-        if (contacts[i].firstName === name && contacts[i].hasOwnProperty(prop)){
-            return contacts[i][prop];
-        } else if (!contacts[i][name]){// !== name){
-            return "No such contact";
-        } else if (!contacts[i].hasOwnProperty(prop)){
-            return "No such property";
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop];
+            } else {
+                return "No such property";
+            }
         }
     }
+    return "No such contact";
+    // Only change code above this line
 }
 
-console.log(lookUpProfile("Kristian", "lastName"));
+console.log(lookUpProfile("Bob", "number"));
