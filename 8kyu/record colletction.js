09cbcd604ef9154bf60510a1e30db7993de3fe3b
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-07 23:02:41 +0500

    add class

--- 8kyu/record colletction.js
+++ 8kyu/record colletction.js
@@ -23,6 +23,8 @@ const recordCollection = {
 function updateRecords(records, id, prop, value) {
     if (prop !== 'tracks' && value !== ''){
         records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+        records[id][prop] = [value];
     } else if (prop === 'tracks' && value !== ''){
         records[id][prop].push(value)
     } else {
