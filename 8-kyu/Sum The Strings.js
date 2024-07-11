750ef0f96772ed13e75a9294c37eda96fd40320f
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-11 14:32:18 +0500

    add task_39

---
+++ 8-kyu/Sum The Strings.js
@@ -0,0 +1,9 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+//
+// Example: (Input1, Input2 -->Output)
+
+function sumStr(a,b) {
+	return String(Number(a) + Number(b))
+}
+
+console.log(sumStr("4","5"))
