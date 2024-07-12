f5b7c0b7851fe3f3e8e2907da5eab210c76d4578
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-12 13:31:32 +0500

    add task_41

--- 8-kyu/Collinearity.js
+++ 8-kyu/Collinearity.js
@@ -4,8 +4,7 @@ function collinearity( x1,y1, x2,y2 ) {
 	if ((x1 === 0 && x2 === 0) || (y1 === 0 && y2 === 0) || (x1 === 0 && y1 === 0) || (x2 === 0 && y2 === 0)) {
 		return true;
 	}
-	console.log(Math.ceil((x1 + y2) / (y1 + x2)) * y1)
-	return (Math.ceil((x1 + y2) / (y1 + x2)) * x1 === x2) && (Math.ceil((x1 + y2) / (y1 + x2)) * y1 === y2);
+	return x1 * y2 === x2 * y1;
 }
 
 console.log(collinearity(3,4,9,12));
