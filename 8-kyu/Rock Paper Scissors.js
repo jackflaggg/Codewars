00fbcf92862dc489ce01be0e1a16ef539f4a3f4e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-12 15:14:43 +0500

    add task_45-46

--- 8-kyu/Rock Paper Scissors.js
+++ 8-kyu/Rock Paper Scissors.js
@@ -2,17 +2,10 @@
 // Let's play! You have to return which player won! In case of a draw return Draw!.
 
 const rps = (p1, p2) => {
-	if (p1 === p2) {
-		return "Draw!";
-	} else if (
-		(p1 === "rock" && p2 === "scissors") ||
-		(p1 === "scissors" && p2 === "paper") ||
-		(p1 === "paper" && p2 === "rock")
-	) {
-		return "Player 1 won!";
-	} else {
-		return "Player 2 won!";
+	if(p1 === p2) {
+		return 'Draw!'
 	}
+	return `Player ${/rockscissors|scissorspaper|paperrock/.test(p1+p2)? 1 : 2} won!`;
 };
 
 console.log(rps('paper', 'rock'));
